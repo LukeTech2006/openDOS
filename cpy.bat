fa8a34e6b49cfd03a876d90a16ae4738be645141
LukeTech2020 <lukaskretschmar1@gmail.com>
2024-07-08 18:37:24 +0200

    New PC

--- cpy.bat
+++ cpy.bat
@@ -1,2 +1,2 @@
-rmdir /s /q "Ocelot\4b2b14af-af89-4f00-9e78-35e440ac8e24\*"
-xcopy /s /y "openNT Standard" "Ocelot\4b2b14af-af89-4f00-9e78-35e440ac8e24"
+rmdir /s /q "Ocelot\fa831265-b962-47b5-957f-6e167c6e4119\*"
+xcopy /s /y "openNT Standard" "Ocelot\fa831265-b962-47b5-957f-6e167c6e4119"
